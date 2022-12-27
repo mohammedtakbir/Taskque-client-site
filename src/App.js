08f1f0fbb9c07dd9466e5a5b687a5cd6e7f62077
mohammedtakbir <takbirhossain678@gmail.com>
2022-12-27 18:34:02 +0600

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routers/routes";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
